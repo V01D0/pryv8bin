4e4a1ae643f2dc2c4187cacfd86431f6da7d8687
V01D0 <38071738+V01D0@users.noreply.github.com>
2021-03-06 23:05:06 +0530

    added comment to routes file

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -33,6 +33,7 @@ $routes->setAutoRoute(true);
 // route since we don't have to scan directories.
 $routes->get('/', 'Home::index');
 $routes->add('pastes/(:any)', 'Pastes::view/$1');
+// $routes->add('/index.php', 'Home::index');
 // $routes->add('pastes/docs', '/docs');
 /*
  * --------------------------------------------------------------------
