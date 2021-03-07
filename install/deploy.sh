42e3a9bda3e6673038957511518b7a6e342438c0
V01D0 <38071738+V01D0@users.noreply.github.com>
2021-03-07 19:31:21 +0530

    changed path in deploy.sh

--- install/deploy.sh
+++ install/deploy.sh
@@ -1,3 +1,3 @@
 wget -P /tmp/ https://github.com/twbs/bootstrap/releases/download/v5.0.0-beta2/bootstrap-5.0.0-beta2-dist.zip 
 unzip /tmp/bootstrap-5.0.0-beta2-dist.zip -d ../public/
-mv ../public/bootstrap-5.0.0-beta2-dist dist
+mv ../public/bootstrap-5.0.0-beta2-dist ../public/dist
