061242326384946155fdbe31c3da142b11908717
V01D0 <38071738+V01D0@users.noreply.github.com>
2021-03-11 11:32:03 +0530

    removed comments, added model function call

--- app/Controllers/Paste.php
+++ app/Controllers/Paste.php
@@ -13,45 +13,21 @@ class Paste extends Home
     }
     public function index()
     {
+        $db = db_connect();
+        $model = new paste_new($db);
         // $model = \model('App\Models\paste_new');
-        $model = new paste_new();
-        $request = \Config\Services::request();
+        $request = service('request');
         $pasteVars = $request->getPost();
         if (!$request->getPost('submit') && $request->getPost('submit') != 'Submit') {
             echo "ERROR";
             return view('error');
         }
-        // $nulls = array();
-        // foreach ($pasteVars as $var => $val) {
-        //     echo "key = " . $var;
-        //     echo "<br>";
-        //     if (trim($val) == "") {
-        //         $val = "NULL";
-        //     }
-        //     echo "value = " . $val;
-        //     echo "<br>";
-        //     // if (trim($val) == "") {
-        //     //     array_push($nulls, $var);
-        //     // }
-        //     // if (in_array($var, $nulls)) {
-        //     //     $val = NULL;
-        //     // }
-        // }
+        // echo $pasteVars['expiry'];
         // return view('result');
-        // print_r($pasteVars);
-        // switch($pasteVars['expiry']) {
-        //     case "never":
-        //         $pasteVars['expiry'] = NULL;
-        //         break;
-        //     case "bar":
-        //         $pasteVars['expiry'] = "bar";
-        //         break;
-        //     case "d1":
-        //         $pasteVars['expiry'] = "";
-        // }
-
-        $model->parsePaste($pasteVars['paste_content'], $pasteVars['expiry'], $pasteVars['title'], $pasteVars['password']);
-        // return view('result');
+        $exp = $model->parsePaste($pasteVars);
+        var_dump($exp);
+        return view('error');
+        // $model->parsePaste($pasteVars);
     }
 
     public function view($page = 'index')
