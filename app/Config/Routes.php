067afcb24966a5b20116fb95b0e42d6f4e3ffb64
V01D0 <38071738+V01D0@users.noreply.github.com>
2021-03-28 18:15:39 +0530

    added error page route

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -32,6 +32,7 @@ $routes->setAutoRoute(true);
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
 $routes->get('/', 'Home::index');
+$routes->add('/error', 'Index::error');
 $routes->add('pastes/(:any)', 'Pastes::view/$1');
 // $routes->add('/index.php', 'Home::index');
 // $routes->add('pastes/docs', '/docs');
