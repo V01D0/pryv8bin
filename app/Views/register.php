68637e6269e831e788ff88da76c5c0a72a8b3319
V01D0 <38071738+V01D0@users.noreply.github.com>
2021-03-30 18:50:04 +0530

    changed session var

--- app/Views/register.php
+++ app/Views/register.php
@@ -4,9 +4,9 @@
             <div class="container">
                 <h3>Register</h3>
                 <hr>
-                <?php if (session()->get('success')) : ?>
+                <?php if (session()->get('logged')) : ?>
                     <div class="alert alert-success" role="alert">
-                        <?= session()->get('success') ?>
+                        <?= session()->get('loggedin') ?>
                     </div>
                 <?php endif; ?>
                 <?= form_open('register') ?>
