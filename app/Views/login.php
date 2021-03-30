f390e2e5c2a2b979d3b3c86923cc1117ba08074c
V01D0 <38071738+V01D0@users.noreply.github.com>
2021-03-30 17:26:23 +0530

    changed to previous controller

--- app/Views/login.php
+++ app/Views/login.php
@@ -10,7 +10,7 @@
                     </div>
                 <?php endif; ?>
                 <div class="container box">
-                <?= form_open('users/login') ?>
+                <?= form_open('login') ?>
                         <div class="form-group">
                             <label for="email">Email address</label>
                             <?= form_input('email', '', 'id="email" class="form-control"', 'email') ?>
