9922490720352167c23b08f66d094ece77cd5950
V01D0 <38071738+V01D0@users.noreply.github.com>
2021-04-01 17:32:41 +0530

    added template to display pastes

--- app/Views/result.php
+++ app/Views/result.php
@@ -1,15 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Result</title>
-</head>
-
-<body>
-
-</body>
-
-</html>
+<div class="col-10">
+	<textarea class="form-control" name="paste_content" style="  color: white;
+	height: 720px;
+	background-color: rgb(51, 51, 51);
+	resize: none;
+	border-color: yellowgreen;"
+	readonly><?= $paste ?></textarea>
+</div>
