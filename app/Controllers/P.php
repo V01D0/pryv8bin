6ff5c3690d33dfb48f6bc12e93f07264fa83d274
V01D0 <38071738+V01D0@users.noreply.github.com>
2021-04-01 17:32:19 +0530

    added controller to view pastes

---
+++ app/Controllers/P.php
@@ -0,0 +1,45 @@
+<?php
+
+    namespace App\Controllers;
+    use App\Models\view_paste;
+
+	class P extends Home
+    {
+        function __construct()
+        {
+            parent::__construct();
+        }
+		
+		public function index()
+        {
+            $uri = current_url(true);
+            if(trim(empty($uri->getSegment(2))))
+                return view("error");
+            else
+            {
+				if($uri->getTotalSegments()>2)
+				{
+					throw \CodeIgniter\Exceptions\PageNotFoundException::forPageNotFound();
+				}
+                $link = trim(strtolower($uri->getSegment(2)));
+                $db = db_connect();
+                $model = new view_paste($db);
+				$res = $model->getPaste($link);
+				if(!$res)
+				{
+					throw \CodeIgniter\Exceptions\PageNotFoundException::forPageNotFound();
+				}
+				else
+				{
+					echo view("templates/header");
+					$isLarge = $model->isLarge($link);
+					if($isLarge)
+						$res .= file_get_contents(WRITEPATH . $link . '.txt');
+					echo view("result", [
+						'paste' => $res
+					]);
+					echo view("templates/footer");
+				}
+            }
+        }
+	}
