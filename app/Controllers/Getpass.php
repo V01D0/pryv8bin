745dfa9f524b3e52f5ba3bdf9bd0fd8142a8e790
V01D0 <38071738+V01D0@users.noreply.github.com>
2021-04-01 22:44:47 +0530

    passwords 🔑

---
+++ app/Controllers/Getpass.php
@@ -0,0 +1,36 @@
+<?php
+
+    namespace App\Controllers;
+    use App\Models\view_paste;
+
+	class Getpass extends Home
+    {
+        function __construct()
+        {
+            parent::__construct();
+        }
+		public function index()
+		{
+			$request = service('request');
+			$db = db_connect();
+			$data = $request->getPost();
+			if(!isset($data['password']))
+				return view('error');
+			$link = $data['link'];
+			$password = $data['password']; 
+			$model = new view_paste($db);
+			if($model->verifyPassword($link, $password))
+			{
+				echo view("templates/header");
+				$res = $model->getPaste($link);
+				if($model->isLarge($link))
+					$res .= file_get_contents(WRITEPATH . $link . '.txt');
+				echo view("result", [
+					'paste' => $res
+				]);
+				return view("templates/footer");
+			}
+			return view("error");
+		}
+	}
+		
