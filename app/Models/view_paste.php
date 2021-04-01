b05fcfcccc83c4ff2bb0cfe09787a42f355b3f81
V01D0 <38071738+V01D0@users.noreply.github.com>
2021-04-02 00:44:12 +0530

    cleaned code

--- app/Models/view_paste.php
+++ app/Models/view_paste.php
@@ -10,23 +10,6 @@
             $this->db = &$db;
         }
 
-		public function hasPassword($link)
-		{
-			$query = $this->db->query("SELECT `password` FROM `pastes` WHERE `link`='$link' AND `password` IS NOT NULL");
-			if($query->getNumRows() <= 0)
-				return false;
-			return true;
-		}
-
-		public function getPaste($link)
-		{
-			$query = $this->db->query("SELECT `paste` FROM `pastes` WHERE `link`='$link'");
-			if($query->getNumRows() <= 0)
-				return false;
-			$result = $query->getResultArray();
-			return $result[0]['paste'];
-		}
-
 		public function isLarge($link)
 		{
 			$query = $this->db->query("SELECT `large` FROM `pastes` WHERE `link`='$link'");
@@ -34,6 +17,14 @@
 			if($result[0]['large'] == 1)
 				return true;
 			return false;
+		}
+
+		public function hasPassword($link)
+		{
+			$query = $this->db->query("SELECT `password` FROM `pastes` WHERE `link`='$link' AND `password` IS NOT NULL");
+			if($query->getNumRows() <= 0)
+				return false;
+			return true;
 		}
 
 		public function verifyPassword($link, $password)
@@ -46,4 +37,24 @@
 				return true;
 			return false;
 		}
+
+		public function getPaste($link)
+		{
+			$query = $this->db->query("SELECT `paste` FROM `pastes` WHERE `link`='$link'");
+			if($query->getNumRows() <= 0)
+				return false;
+			$result = $query->getResultArray();
+			return $result[0]['paste'];
+		}
+
+		public function getTitle($link)
+		{
+			$query = $this->db->query("SELECT `title` FROM `pastes` WHERE `link`='$link'");
+			if($query->getNumRows() <= 0)
+				return false;
+			$result = $query->getResultArray();
+			return $result[0]['title'];
+		}
+
+
 	}
