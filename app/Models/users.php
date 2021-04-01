93ca15ed19cba7c7c1284ebad8b99832f9865aa0
V01D0 <38071738+V01D0@users.noreply.github.com>
2021-04-01 17:12:45 +0530

    function visibility

--- app/Models/users.php
+++ app/Models/users.php
@@ -1,7 +1,6 @@
 <?php
 
     namespace App\Models;
-
     use CodeIgniter\Database\ConnectionInterface;
 
 	class users
@@ -11,22 +10,22 @@
             $this->db = &$db;
             // $db = \Config\Database::connect();
         }
-
-		function sendMail($type, $hash, $id, $email)
+		
+		private function sendMail($type, $hash, $id, $email)
 		{
 			switch($type)
 			{
 				case 1:
 					$body = ("Hello").",\n\n"._("You, or someone that knows your email address,")."\n"._("just signed up with paste.pryv8.org")."\n\n";
 					$body .= _("Please click on the following URL to confirm your email address:")."\n\n";
-					$body .= "https:/"."/paste.pryv8.org/verify.php?hash=$hash&uid=$id\n\n";
+					$body .= "https:/"."/paste.pryv8.org/verify?hash=$hash&uid=$id\n\n";
 					mail($email, "[Pryv8bin]: "._("Email Verification Check"), $body, "From: noreply@pryv8.org\nReturn-Path: noreply@pryv8.org","-f noreply@pryv8.org");
 					break;
 
 				case 2:
 					$body = ("Hello").",\n\n"._("You, or someone that knows your email address,")."\n"._("requested to reset your password on paste.pryv8.org")."\n\n";
 					$body .= _("Please click on the following URL to reset your password:")."\n\n";
-					$body .= "https:/"."/paste.pryv8.org/reset.php?hash=$hash&uid=$id\n\n";
+					$body .= "https:/"."/paste.pryv8.org/reset?hash=$hash&uid=$id\n\n";
 					mail($email, "[Pryv8bin]: "._("Password Reset"), $body, "From: noreply@pryv8.org\nReturn-Path: noreply@pryv8.org","-f noreply@pryv8.org");
 					break;
 				
@@ -35,24 +34,23 @@
 			}
 		}
 
-		function verify($hash, $uid)
+		public function verify($hash, $uid)
 		{
 			$query = $this->db->query("SELECT 1 FROM `auth` WHERE `hash`='$hash' AND `uid`= $uid");
 			if($query->getNumRows() <= 0)
 				return false;
-			$builder = $this->db->table("auth");
 			$query = $this->db->query("UPDATE `auth` SET `hash`=NULL WHERE `uid`=$uid");
 			return true;
 		}
 
-		function genHash()
+		private function genHash()
 		{
 			$rnd = fopen("/dev/urandom", "r");
 			$hash = md5(fgets($rnd, 64));
 			return $hash;
 		}
 
-		function registerUser($email, $password)
+		public function registerUser($email, $password)
 		{
 			$hash = $this->genHash();
 			$username = strtok($email, '@'); 
@@ -67,7 +65,7 @@
 			$this->sendMail(1, $hash, $id, $email);
 		}
 
-		function tryLogin($email, $password)
+		public function tryLogin($email, $password)
 		{
 			$query = $this->db->query("SELECT `password` FROM `auth` WHERE `email`='$email'");
 			if($query->getNumRows() <= 0)
@@ -78,14 +76,14 @@
 			return false;
 		}
 
-		function getUsername($email)
+		public function getUsername($email)
 		{
 			$query = $this->db->query("SELECT `username` FROM `auth` WHERE `email`='$email'");
 			$result = $query->getResultArray();
 			return $result[0]['username'];
 		}
 
-		function getUID($email)
+		public function getUID($email)
 		{
 			$query = $this->db->query("SELECT `uid` FROM `auth` WHERE `email`='$email'");
 			$result = $query->getResultArray();
