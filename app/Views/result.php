d7e48088e3b5bd66affd2dfa468bc7243b3fc466
V01D0 <38071738+V01D0@users.noreply.github.com>
2021-04-02 14:09:28 +0530

    added title

--- app/Views/result.php
+++ app/Views/result.php
@@ -1,4 +1,10 @@
-<div class="col-10">
+<?php
+if(isset($title))
+echo '<h3 class="paste__heading">'.$title.'</h3>';
+else
+echo '<h3 class="paste__heading">Untitled paste</h3>';
+?>
+<div class="col-10" style="margin-bottom: 10%">
 	<textarea class="form-control" name="paste_content" style="  color: white;
 	height: 720px;
 	background-color: rgb(51, 51, 51);
