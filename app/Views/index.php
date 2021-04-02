f6323649c00b4ee1e9afb91e9d836e70a67c2070
V01D0 <38071738+V01D0@users.noreply.github.com>
2021-04-02 14:08:39 +0530

    allowed tabs in textarea

--- app/Views/index.php
+++ app/Views/index.php
@@ -179,6 +179,22 @@
 			}
 		});
 
+		document.getElementById('paste-text').addEventListener('keydown', function(e) {
+			if (e.key == 'Tab') {
+				e.preventDefault();
+				var start = this.selectionStart;
+				var end = this.selectionEnd;
+
+				// set textarea value to: text before caret + tab + text after caret
+				this.value = this.value.substring(0, start) +
+				"\t" + this.value.substring(end);
+
+				// put caret at right position again
+				this.selectionStart =
+				this.selectionEnd = start + 1;
+			}
+			});
+
 		cb = document.getElementById("burn-after-read");
 		cb.addEventListener('change', function() {
 			if (this.checked) {
