d9a6a5b26bb1dec59099008ca783ba027e2136a6
V01D0 <38071738+V01D0@users.noreply.github.com>
2021-04-24 15:28:06 +0530

    added burn and error code

--- app/Controllers/Getpass.php
+++ app/Controllers/Getpass.php
@@ -1,7 +1,8 @@
 <?php
 
     namespace App\Controllers;
-    use App\Models\view_paste;
+	use App\Models\paste_new;
+	use App\Models\view_paste;
 
 	class Getpass extends Home
     {
@@ -13,35 +14,40 @@
 		{
 			$request = service('request');
 			$db = db_connect();
+			$model = new view_paste($db);
 			$data = $request->getPost();
+
+			//IF PASTE IS ONLY BURN AFTER READ
+			if((!isset($data['link']) && isset($data['burnit'])))
+			{
+				$link = $data['burnit'];
+				$model = new paste_new($db);
+				if(!$model->isNameInDB($link))
+					return view("error", [
+						"reason" => "The paste you are looking for does not exist" 
+					]);
+				session()->set([$link=>true]);
+				return redirect()->to("p/$link");
+			}
+
 			if(!isset($data['password']))
 				return view('error',[
 					"reason" => "Invalid password"
 				]);
+
 			$link = $data['link'];
 			$password = $data['password']; 
-			$model = new view_paste($db);
-			// if($model->verifyPassword($link, $password))
-			// {
-			// 	echo view("templates/header");
-			// 	$res = $model->getPaste($link);
-			// 	if($model->isLarge($link))
-			// 		$res .= file_get_contents(WRITEPATH . $link . '.txt');
-			// 	echo view("result", [
-			// 		'paste' => $res
-			// 	]);
-			// 	return view("templates/footer");
-			// }
-			// echo view("templates/header");
-			// echo view("askpass", [
-			// 	'link'=> $link
-			// ]);
-			// return view("templates/footer");
+			//IF PASSWORD MATCHES
 			if($model->verifyPassword($link, $password))
 			{
 				session()->set([$link=>true]);
+				$model->updateViews($link);
 				return redirect()->to("p/$link");
 			}
+			//ELSE
+			return view('error',[
+				"reason" => "Invalid password"
+			]);
 		}
 	}
 		
