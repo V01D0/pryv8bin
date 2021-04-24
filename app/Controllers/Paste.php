34159c0f4d5f2b35fe9bd0e2280d20dd4e49e747
V01D0 <38071738+V01D0@users.noreply.github.com>
2021-04-24 15:29:40 +0530

    cleaned up the code

--- app/Controllers/Paste.php
+++ app/Controllers/Paste.php
@@ -3,7 +3,6 @@
 namespace App\Controllers;
 
 use App\Models\paste_new;
-use CodeIgniter\Controller;
 
 class Paste extends Home
 {
@@ -17,39 +16,14 @@ class Paste extends Home
         $db = db_connect();
         //CREATE MODEL VARIABLE
         $model = new paste_new($db);
-        // $model = \model('App\Models\paste_new');
+
         $request = service('request');
         $pasteVars = $request->getPost();
         if ($pasteVars['submit'] !== 'Submit')
-        {
             return view('error');
-        }
-        // echo $pasteVars['expiry'];
-        // return view('result');
-        // $ex = $model->parsePaste($pasteVars);
-        // $ex = json_decode(json_encode($ex), true);
-        // foreach ($ex as $key => $value) {
-        //     $ex = $value;
-        // }
-        // echo $ex;
 
         $link = $model->parsePaste($pasteVars);
-        // return view('result');
-        // $model->parsePaste($pasteVars);
+
         return redirect()->to("p/$link");
-    }
-
-    public function view($page = 'index')
-    {
-        if (!is_file(APPPATH . '/Views/pages/' . $page . '.php')) {
-            // Whoops, we don't have a page for that!
-            throw new \CodeIgniter\Exceptions\PageNotFoundException($page);
-        }
-
-        $data['title'] = ucfirst($page); // Capitalize the first letter
-
-        echo view('templates/header', $data);
-        echo view('pages/' . $page, $data);
-        echo view('templates/footer', $data);
     }
 }
