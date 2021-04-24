41b97b9025de4ad3936d19d168ee00e0dd3f83bd
V01D0 <38071738+V01D0@users.noreply.github.com>
2021-04-24 15:16:32 +0530

    added a burnLink function and added function description comments

--- app/Models/view_paste.php
+++ app/Models/view_paste.php
@@ -1,9 +1,8 @@
 <?php
 
     namespace App\Models;
-
-use App\Controllers\P;
-use CodeIgniter\Database\ConnectionInterface;
+	use App\Controllers\P;
+	use CodeIgniter\Database\ConnectionInterface;
 
 	class view_paste
 	{
@@ -14,6 +13,7 @@ use CodeIgniter\Database\ConnectionInterface;
 
 		public function isLarge($link)
 		{
+			/*CHECK IF A PASTE IS LARGER THAN 1KB*/
 			$query = $this->db->query("SELECT `large` FROM `pastes` WHERE `link`='$link'");
 			$result = $query->getResultArray();
 			if($result[0]['large'] == 1)
@@ -21,8 +21,19 @@ use CodeIgniter\Database\ConnectionInterface;
 			return false;
 		}
 
+		public function isBurn($link)
+		{
+			/*CHECK IF A LINK HAS BURN AFTER READ*/
+			$query = $this->db->query("SELECT `burn` FROM `pastes` WHERE `link`='$link'");
+			$result = $query->getResultArray();
+			if($result[0]['burn'] == 1)
+				return true;
+			return false;
+		}
+
 		public function hasPassword($link)
 		{
+			/*CHECK IF A CERTAIN LINK HAS PASSWORD*/
 			$query = $this->db->query("SELECT `password` FROM `pastes` WHERE `link`='$link' AND `password` IS NOT NULL");
 			if($query->getNumRows() <= 0)
 				return false;
@@ -31,6 +42,7 @@ use CodeIgniter\Database\ConnectionInterface;
 
 		public function verifyPassword($link, $password)
 		{
+			/*CHECK IF PASSWORD MATCHES FOR A GIVEN LINK*/
 			$query = $this->db->query("SELECT `password` FROM `pastes` WHERE `link`='$link'");
 			if($query->getNumRows() <= 0)
 				return false;
@@ -42,6 +54,7 @@ use CodeIgniter\Database\ConnectionInterface;
 
 		public function getPaste($link)
 		{
+			/*GET PASTE CONTENT (FIRST 1024 CHARS) FOR A CERTAIN PASTE*/
 			$query = $this->db->query("SELECT `paste` FROM `pastes` WHERE `link`='$link'");
 			if($query->getNumRows() <= 0)
 				return false;
@@ -51,6 +64,7 @@ use CodeIgniter\Database\ConnectionInterface;
 
 		public function getTitle($link)
 		{
+			/*GET THE TITLE FOR A CERTAIN PASTE*/
 			$query = $this->db->query("SELECT `title` FROM `pastes` WHERE `link`='$link'");
 			if($query->getNumRows() <= 0)
 				return false;
@@ -60,6 +74,7 @@ use CodeIgniter\Database\ConnectionInterface;
 
 		public function getViews($link)
 		{
+			/*GET NUMBER OF VIEWS FOR A CERTAIN PASTE*/
 			$query = $this->db->query("SELECT `views` FROM `pastes` WHERE `link`='$link'");
 			$result = $query->getResultArray();
 			return $result[0]['views'];
@@ -67,15 +82,16 @@ use CodeIgniter\Database\ConnectionInterface;
 
 		public function updateViews($link)
 		{
+			/*INCREMENT THE VIEWS OF A PASTE BY 1*/
 			$sql = "UPDATE `pastes` SET `views`=views+1 WHERE link='$link'";
 			if($this->db->query($sql))
 				return true;
-			else
-				return false;
+			return false;
 		}
 
 		public function getAuthor($link)
 		{
+			/*GET THE AUTHOR OF THE PASTE*/
 			$query = $this->db->query("SELECT `uid` FROM `pastes` WHERE `link`='$link'");
 			$result = $query->getResultArray();
 			if($result[0]['uid'] == 0)
@@ -83,5 +99,17 @@ use CodeIgniter\Database\ConnectionInterface;
 			$query = $this->db->query("SELECT `username` FROM `auth` WHERE `uid`=$result[0]['uid']");
 			$result = $query->getResultArray();
 			return $result[0]['username'];
+		}
+
+		public function burnLink($link)
+		{
+			/*NUKE THE PASTE*/
+			if($this->isBurn($link))
+			{
+				if($this->isLarge($link))
+					unlink(WRITEPATH . '/' . $link . '.txt');
+			}
+			$query = $this->db->query("DELETE FROM `pastes` WHERE `link`='$link'");
+			return true;
 		}
 	}
