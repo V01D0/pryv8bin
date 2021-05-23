90c7b188ebd8c44d4d97c8c7fca5dfad6bedf67e
V01D0 <38071738+V01D0@users.noreply.github.com>
2021-05-23 17:34:19 +0530

    created function to match password

--- app/Models/users.php
+++ app/Models/users.php
@@ -35,6 +35,15 @@ class users
 			}
 		}
 
+		public function isMatch($email, $givenPassword)
+		{
+			$query = $this->db->query("SELECT `password` FROM `auth` WHERE `email`=$email");
+			$result = $query->getResultArray();
+			$hash = $result[0]['password'];
+			$result = password_verify($givenPassword, $hash);
+			return $result;
+		}
+
 		public function verify($hash, $uid, $type)
 		{
 			if($type == 'v')
