572ebf64247ed3f251a552c093d15cdb95d769c3
V01D0 <38071738+V01D0@users.noreply.github.com>
2021-06-08 13:42:00 +0530

    styles

--- app/Views/api.php
+++ app/Views/api.php
@@ -7,35 +7,35 @@
 
 <p class="paste__heading">Requirements <br> - an API key (get yours<a href="/api/get"> here</a>)</p>
 
-<h3 class="paste__heading">
+<h3 class="api__heading">
 API URL
 </h3>
 <code>
 	<a href="<?=site_url()."api"?>"><?=site_url()."api/"?></a>
 </code>
 <br><br>
-<h3 class="paste__heading">
+<h3 class="api__heading">
 Create paste
 </h3>
 <code>
 	<a href="<?=site_url()."api/create"?>"><?=site_url()."api/create/"?></a>
 </code>
 <br><br>
-<h3 class="paste__heading">
+<h3 class="api__heading">
 Read paste
 </h3>
 <code>
 	<a href="<?=site_url()."api/paste/"?>"><?=site_url()."api/read/<pasteID>"?></a>
 </code>
 <br><br>
-<h3 class="paste__heading">
+<h3 class="api__heading">
 Update paste
 </h3>
 <code>
 	<a href="<?=site_url()."api/update/"?>"><?=site_url()."api/update/<pasteID>"?></a>
 </code>
 <br><br>
-<h3 class="paste__heading">
+<h3 class="api__heading">
 Delete paste
 </h3>
 <code>
